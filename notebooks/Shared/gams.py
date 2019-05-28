37c3e7aed2fc46c322375e403926a1ce72758def
juanpalomares <48070994+juanpalomares@users.noreply.github.com>
2019-05-28 17:29:52 -0500

    GAMS- PREBA 5:29

---
+++ notebooks/Shared/gams.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+dbu
+
+# COMMAND ----------
+
+import os
+os.chdir('/opt/gams/gams27.1_linux_x64_64_sfx/apifiles/Python/api')
+os.environ['LD_LIBRARY_PATH'] = '/opt/gams/gams27.1_linux_x64_64_sfx/'
+
+# COMMAND ----------
+
+# MAGIC %%bash
+# MAGIC python setup.py install
+
+# COMMAND ----------
+
+# MAGIC %%bash
+# MAGIC python /opt/gams/gams27.1_linux_x64_64_sfx/apifiles/Python/transport1.py
